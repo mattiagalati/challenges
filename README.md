78dd8677165f199dbbeb72c9b651caa94605feb3
Mattia Galati <mattia.galati@gisdata.it>
2021-10-05 23:12:24 +0200

    [Javascript + HTML (Livello 1)] v20211005

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Test tecnici
 
 - [Python + MySQL](python_mysql/README.md)
+- [Javascript + HTML (Livello 1)](javascript_html_1/README.md)
---
+++ javascript_html_1/app.js
@@ -0,0 +1,11 @@
+// Mantenere questa dichiarazione di funzione
+const process = () => {
+  
+  /*
+  Qui dovrai scrivere il tuo algoritmo
+  */
+
+  // Dopo aver analizzato tutte le parole, quelle che hanno superato
+  // i criteri, vanno stampate nella pagina HTML, una per ogni riga
+  document.getElementById("response").innerHTML = '...';
+};
